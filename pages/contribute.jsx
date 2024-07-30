8e550da56a6b786410c59a995bd0ae831239596b
abhijeetwaghchaurecodes <abhijeetswaghchaure@gmail.com>
2024-07-30 16:00:22 +0530

    basic structure of the website
    this commit contains the basic structure of the website.

---
+++ pages/contribute.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Contact from '../components/Contact'
+import Hero from '../components/Hero'
+
+const libraries = () => {
+  return (
+    <div>
+        <Hero heading='Contribute' message='Donate!' />
+        <Contact />
+    </div>
+  )
+}
+
+export default libraries
